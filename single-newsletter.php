cbbf41a9de9b7c66e219306d859ab13c19e727e0
Jesse Gardner <jesse@plasticmind.com>
2021-06-28 09:09:49 -0400

    Updated newsletter image reference

--- single-newsletter.php
+++ single-newsletter.php
@@ -565,7 +565,7 @@
                               <td class="mcnImageContent" valign="top" style="padding-right: 9px; padding-left: 9px; padding-top: 0; padding-bottom: 0; text-align:center;">
                                   
                                       
-                                          <img align="center" alt="" src="https://gallery.mailchimp.com/1d974b3c3d4644e32acdefc5e/images/7f557c32-c844-4bcc-929d-e2f9e42c31bd.jpg" width="564" style="max-width:900px; padding-bottom: 0; display: inline !important; vertical-align: bottom;" class="mcnImage">
+                                          <img align="center" alt="" src="https://mcusercontent.com/1d974b3c3d4644e32acdefc5e/images/36a604f5-418f-f178-90cd-59718f4de098.png" width="564" style="max-width:900px; padding-bottom: 0; display: inline !important; vertical-align: bottom;" class="mcnImage">
                                       
                                   
                               </td>
